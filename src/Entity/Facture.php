00d167b06bcd1b36f4c1537298da395e713a44d1
dhifaoui achref <achref.dhifaoui00@gmail.com>
2024-04-26 04:21:55 +0200

    #4 create entity Facture and LigneFacture

---
+++ src/Entity/Facture.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FactureRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FactureRepository::class)]
+class Facture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $numero = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date_facture = null;
+
+    #[ORM\ManyToOne(inversedBy: 'factures')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Client $client = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumero(): ?string
+    {
+        return $this->numero;
+    }
+
+    public function setNumero(string $numero): static
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getDateFacture(): ?\DateTimeInterface
+    {
+        return $this->date_facture;
+    }
+
+    public function setDateFacture(\DateTimeInterface $date_facture): static
+    {
+        $this->date_facture = $date_facture;
+
+        return $this;
+    }
+
+    public function getClient(): ?Client
+    {
+        return $this->client;
+    }
+
+    public function setClient(?Client $client): static
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+}
---
+++ src/Entity/LigneFacture.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LigneFactureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LigneFactureRepository::class)]
+class LigneFacture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    private ?FormationAssurer $designation = null;
+
+    #[ORM\Column]
+    private ?int $quantite = null;
+
+    #[ORM\Column]
+    private ?float $prixUnitaire = null;
+
+    #[ORM\ManyToOne]
+    private ?Unite $Unite = null;
+
+    #[ORM\Column]
+    private ?float $totalHT = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDesignation(): ?FormationAssurer
+    {
+        return $this->designation;
+    }
+
+    public function setDesignation(?FormationAssurer $designation): static
+    {
+        $this->designation = $designation;
+
+        return $this;
+    }
+
+    public function getQuantite(): ?int
+    {
+        return $this->quantite;
+    }
+
+    public function setQuantite(int $quantite): static
+    {
+        $this->quantite = $quantite;
+
+        return $this;
+    }
+
+    public function getPrixUnitaire(): ?float
+    {
+        return $this->prixUnitaire;
+    }
+
+    public function setPrixUnitaire(float $prixUnitaire): static
+    {
+        $this->prixUnitaire = $prixUnitaire;
+
+        return $this;
+    }
+
+    public function getUnite(): ?Unite
+    {
+        return $this->Unite;
+    }
+
+    public function setUnite(?Unite $Unite): static
+    {
+        $this->Unite = $Unite;
+
+        return $this;
+    }
+
+    public function getTotalHT(): ?float
+    {
+        return $this->totalHT;
+    }
+
+    public function setTotalHT(float $totalHT): static
+    {
+        $this->totalHT = $totalHT;
+
+        return $this;
+    }
+}
